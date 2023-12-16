6cdc7799064b9450c9f75b17db9c59690eb47609
suprajgowda <153866276+suprajgowda@users.noreply.github.com>
2023-12-17 00:35:51 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import ResponsiveAppBar from "./ResponsiveAppBar";
+import ResponsiveAppBar2 from "./ResponsiveAppBar2";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <ResponsiveAppBar />
+      <ResponsiveAppBar2 />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer.tsx
@@ -0,0 +1,274 @@
+import * as React from "react";
+import { Container, Grid, IconButton, Link, Typography } from "@mui/material";
+import FacebookSharpIcon from "@mui/icons-material/FacebookSharp";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+
+function Copyright(props: any) {
+  return (
+    <Grid
+      container
+      spacing={2}
+      justifyContent="space-between"
+      sx={{
+        borderTop: `1px solid #353a49`,
+      }}
+    >
+      <Grid item xs={6} sm={3}>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          align="center"
+          sx={{
+            mt: 2,
+            display: "flex",
+            justifyContent: "flex-start",
+            color: "#b5b8be",
+          }}
+        >
+          {"Copyright © "}
+          <Link color="inherit" sx={{ mr: 0.25, color: "#b5b8be" }}>
+            Your Website
+          </Link>
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Grid>
+
+      <Grid item xs={6} sm={3}>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          align="center"
+          sx={{
+            mt: 2,
+            textDecoration: "none",
+            display: "flex",
+            justifyContent: "flex-end",
+            color: "#b5b8be",
+          }}
+        >
+          <Link
+            color="inherit"
+            sx={{ textDecoration: "none", mr: 2, color: "#b5b8be" }}
+          >
+            Terms of Use
+          </Link>
+          <Link
+            color="inherit"
+            sx={{ textDecoration: "none", color: "#b5b8be" }}
+          >
+            Privacy Policy
+          </Link>
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
+
+function Footer() {
+  return (
+    <Container
+      maxWidth="xl"
+      component="footer"
+      sx={{
+        borderTop: `1px solid black`,
+        mt: 8,
+        pt: 5,
+        pb: 3,
+        px: 6,
+        backgroundColor: "#1e2434",
+      }}
+    >
+      <Grid container spacing={4} justifyContent="space-evenly">
+        <Grid item xs={6} sm={3} key={"Company"}>
+          <Typography
+            variant="h6"
+            color="text.primary"
+            sx={{ color: "#fff" }}
+            gutterBottom
+          >
+            MAPLE
+          </Typography>
+          <ul
+            style={{
+              listStyle: "none",
+              paddingLeft: 0,
+              color: "#fff",
+            }}
+          >
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#fff" }}
+              >
+                Lorem ipsum dolor sit ametium, consectetur adipiscing elit.
+              </Link>
+            </li>
+            <li>
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <FacebookSharpIcon />
+              </IconButton>
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <TwitterIcon />
+              </IconButton>
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <InstagramIcon />
+              </IconButton>
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <WhatsAppIcon />
+              </IconButton>
+            </li>
+          </ul>
+        </Grid>
+
+        <Grid item xs={6} sm={3} key={"Company"}>
+          <Typography
+            variant="h6"
+            color="text.primary"
+            sx={{
+              color: "#fff",
+            }}
+            gutterBottom
+          >
+            Contact Us
+          </Typography>
+          <ul
+            style={{
+              listStyle: "none",
+              paddingLeft: 0,
+              color: "#fff",
+            }}
+          >
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#fff" }}
+              >
+                Email: Info.deercreative@gmail.com
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#fff" }}
+              >
+                Phone: +(88) 111 555 666
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#fff" }}
+              >
+                40 Baria Sreet 133/2 New York City, United States
+              </Link>
+            </li>
+          </ul>
+        </Grid>
+
+        <Grid item xs={6} sm={3} key={"Company"}>
+          <Typography
+            variant="h6"
+            color="text.primary"
+            sx={{
+              color: "#fff",
+            }}
+            gutterBottom
+          >
+            Contact Us
+          </Typography>
+          <ul
+            style={{
+              listStyle: "none",
+              paddingLeft: 0,
+              color: "#fff",
+            }}
+          >
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#b5b8be" }}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#b5b8be" }}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#b5b8be" }}
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#b5b8be" }}
+              >
+                Features
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#b5b8be" }}
+              >
+                Courses
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#b5b8be" }}
+              >
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#b5b8be" }}
+              >
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ textDecoration: "none", color: "#b5b8be" }}
+              >
+                FAQs
+              </Link>
+            </li>
+          </ul>
+        </Grid>
+      </Grid>
+
+      <Copyright />
+    </Container>
+  );
+}
+
+export default Footer;
