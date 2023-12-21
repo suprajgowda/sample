b0b07b5cbd4825975bc0222840a47dfdb06b38e1
suprajgowda <suprajsupk@gmail.com>
2023-12-22 04:06:59 +0530

    added home page content

--- src/Home.tsx
+++ src/Home.tsx
@@ -65,20 +65,80 @@ function Home() {
           gutterBottom
           sx={{ textAlign: "center", color: "#384158", mb: 2, mt: 11 }}
         >
-          Welcome To Unicat E-Learning
+          Welcome to Maple - Where Innovation Meets Opportunity!
         </Typography>
+
         <Typography
           variant="body2"
           gutterBottom
-          sx={{ textAlign: "center", color: "#76777a", mb: 5 }}
+          sx={{ textAlign: "center", color: "#76777a", mx: 5, mb: 5 }}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel
-          gravida arcu. Vestibulum feugiat, sapien ultrices fermentum congue,
-          quam velit venenatis sem
+          At MAPLE , we extend a warm and enthusiastic welcome to all aspiring
+          minds ready to embark on a transformative journey. We believe in the
+          power of innovation and the endless possibilities that arise when
+          curiosity meets real-world experience.
+        </Typography>
+
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{ textAlign: "center", color: "#76777a", mx: 5, mb: 5 }}
+        >
+          As you step into our digital space, you enter a realm where
+          theoretical knowledge transforms into practical skills, and classrooms
+          extend beyond walls into the dynamic landscapes of AI, Machine
+          Learning, Blockchain, Robotics, and more. Here, we are not just a
+          startup; we are a community of forward-thinkers, tech enthusiasts, and
+          learners ready to shape the future.
+        </Typography>
+
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{ textAlign: "center", color: "#76777a", mx: 5, mb: 5 }}
+        >
+          Our commitment is simple: to provide students with live projects that
+          go beyond traditional learning. These are not mere assignments; they
+          are opportunities to explore, experiment, and contribute to the
+          ever-evolving world of technology. Whether you are passionate about
+          unraveling the mysteries of Neural Networks, exploring the potential
+          of Blockchain, or pushing the boundaries of Cloud Computing,MAPLE is
+          your platform for growth.
+        </Typography>
+
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{ textAlign: "center", color: "#76777a", mx: 5, mb: 5 }}
+        >
+          We invite you to discover the domains of expertise that await you,
+          guided by industry experts and a collaborative learning environment.
+          Our projects are designed not only to equip you with industry-relevant
+          skills but also to ignite your passion for innovation.
+        </Typography>
+
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{ textAlign: "center", color: "#76777a", mx: 5, mb: 5 }}
+        >
+          Join us on this exciting journey where ideas take flight, and the
+          future is shaped by the collective brilliance of aspiring minds.
+          Welcome toMAPLE - where learning knows no boundaries, and your
+          potential becomes the driving force for tomorrow's technology.
+        </Typography>
+
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{ textAlign: "center", color: "#76777a", mx: 5, mb: 5 }}
+        >
+          Let's embark on this adventure together. Ignite your mind, power your
+          future!
         </Typography>
       </Box>
 
-      <Grid
+      {/* <Grid
         container
         spacing={{ xs: 2, md: 4 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
@@ -250,7 +310,7 @@ function Home() {
             View All Courses
           </Button>
         </Box>
-      </Box>
+      </Box> */}
 
       <NewsLetter />
       <Footer />
