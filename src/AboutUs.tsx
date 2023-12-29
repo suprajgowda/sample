c869a1bbdb13368ca428aac0c009cdeab0cca35d
suprajgowda <suprajsupk@gmail.com>
2023-12-29 20:36:21 +0530

    Added Images to about us page

--- src/AboutUs.tsx
+++ src/AboutUs.tsx
@@ -4,6 +4,16 @@ import ResponsiveAppBar2 from "./ResponsiveAppBar2";
 import NewsLetter from "./NewsLetter";
 import Footer from "./Footer";
 import { Box, Typography } from "@mui/material";
+import Adaptability from "./assets/Adaptability.jpg";
+import Empowerment from "./assets/Empowerment.jpg";
+import Career_Acceleration from "./assets/Career_Acceleration.jpg";
+import Continuous_Learning from "./assets/Continuous-Learning.jpg";
+import Collaborative from "./assets/Collaborative Learning Environment.jpg";
+import Endless_Possibilities from "./assets/Endless-Possibilities.jpg";
+import Expert_Guidance from "./assets/Expert-Guidance.jpg";
+import Fun_and_Enjoyment from "./assets/Fun-and-Enjoyment.jpg";
+import Industry_Relevant_Skills from "./assets/Industry-Relevant-Skills.jpg";
+import Live_Projects from "./assets/Live_Projects.jpg";
 
 const listOfAboutUs = [
   {
@@ -32,22 +42,27 @@ const listOfAboutUs1 = [
   {
     key1: "Inclusivity:",
     key2: " Celebrating diversity and creating an inclusive environment.",
+    image: Empowerment,
   },
   {
     key1: "Continuous Learning:",
     key2: " Encouraging ongoing knowledge-seeking and adaptability.",
+    image: Continuous_Learning,
   },
   {
     key1: "Empowerment:",
     key2: " Fostering autonomy, initiative, and leadership.",
+    image: Empowerment,
   },
   {
     key1: "Flexibility and Adaptability:",
     key2: " Navigating change with resilience and responsivenes.",
+    image: Adaptability,
   },
   {
     key1: "Fun and Enjoyment:",
     key2: " Creating a positive and enjoyable atmosphere for a vibrant community.",
+    image: Fun_and_Enjoyment,
   },
 ];
 
@@ -55,26 +70,32 @@ const listOfAboutUs2 = [
   {
     key1: "Live Projects, Real Experience:",
     key2: "We understand the importance of practical knowledge in today's fast-evolving tech landscape. Our live projects ensure that students don't just learn theories but also get hands-on experience in solving real-world challenges.",
+    image: Live_Projects,
   },
   {
     key1: "Diverse Domains, Endless Possibilities:",
     key2: "Our projects span across diverse domains, including AI, ML, Neural Networks, Robotics, and Thermal Technology. This variety allows students to explore their interests and choose projects that align with their career goals.",
+    image: Endless_Possibilities,
   },
   {
     key1: "Expert Guidance:",
     key2: "At MAPLE , we have a team of experienced professionals and mentors who guide students throughout their project journey. Our experts bring industry insights and academic expertise to ensure a comprehensive learning experience.",
+    image: Expert_Guidance,
   },
   {
     key1: "Collaborative Learning Environment:",
     key2: "We believe in the power of collaboration. Our projects are designed to encourage teamwork, communication, and innovation. Students work together, share ideas, and learn from each other, fostering a collaborative learning environment.",
+    image: Collaborative,
   },
   {
     key1: "Industry-Relevant Skills:",
     key2: "Our projects are carefully curated to equip students with skills that are highly sought after in the industry. Whether it's programming in Python, designing neural networks, or developing robotic systems, our projects focus on building practical, industry-relevant skills.",
+    image: Industry_Relevant_Skills,
   },
   {
     key1: "Career Acceleration:",
     key2: "MAPLE isn't just about completing projects; it's about accelerating your career. The experience gained through our live projects sets students apart in the job market, making them stand out to potential employers.",
+    image: Career_Acceleration,
   },
 ];
 
@@ -172,19 +193,55 @@ export default function AboutUs() {
             </Typography>
           </Box>
 
-          {listOfAboutUs1.map((aboutUs) => {
+          {listOfAboutUs1.map((aboutUs, index) => {
+            let imageBoxStyle: any = {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            };
+
+            if (index % 2 === 0) {
+              imageBoxStyle["pr"] = 2;
+            } else {
+              imageBoxStyle["pl"] = 2;
+            }
+            const imageStyle = { maxWidth: "500px", borderRadius: "5px" };
+
             return (
-              <Box sx={{ mb: 4 }}>
-                <Typography variant="h6" gutterBottom>
-                  {aboutUs.key1}
-                </Typography>{" "}
-                <Typography
-                  variant="subtitle1"
-                  gutterBottom
-                  sx={{ color: "#76777a" }}
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  mb: 8,
+                  flexDirection: index % 2 === 0 ? "row" : "row-reverse",
+                }}
+              >
+                <Box sx={imageBoxStyle}>
+                  <img
+                    src={aboutUs.image}
+                    alt={aboutUs.key1}
+                    style={imageStyle}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    mb: 4,
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                  }}
                 >
-                  {aboutUs.key2}
-                </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    {aboutUs.key1}
+                  </Typography>{" "}
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom
+                    sx={{ color: "#76777a" }}
+                  >
+                    {aboutUs.key2}
+                  </Typography>
+                </Box>
               </Box>
             );
           })}
@@ -205,19 +262,55 @@ export default function AboutUs() {
             </Typography>
           </Box>
 
-          {listOfAboutUs2.map((aboutUs) => {
+          {listOfAboutUs2.map((aboutUs, index) => {
+            let imageBoxStyle: any = {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            };
+
+            if (index % 2 === 0) {
+              imageBoxStyle["pr"] = 2;
+            } else {
+              imageBoxStyle["pl"] = 2;
+            }
+            const imageStyle = { maxWidth: "500px", borderRadius: "5px" };
+
             return (
-              <Box sx={{ mb: 4 }}>
-                <Typography variant="h6" gutterBottom>
-                  {aboutUs.key1}
-                </Typography>{" "}
-                <Typography
-                  variant="subtitle1"
-                  gutterBottom
-                  sx={{ color: "#76777a" }}
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  mb: 8,
+                  flexDirection: index % 2 === 0 ? "row" : "row-reverse",
+                }}
+              >
+                <Box sx={imageBoxStyle}>
+                  <img
+                    src={aboutUs.image}
+                    alt={aboutUs.key1}
+                    style={imageStyle}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    mb: 4,
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                  }}
                 >
-                  {aboutUs.key2}
-                </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    {aboutUs.key1}
+                  </Typography>{" "}
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom
+                    sx={{ color: "#76777a" }}
+                  >
+                    {aboutUs.key2}
+                  </Typography>
+                </Box>
               </Box>
             );
           })}
