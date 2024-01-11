2d3c7418a3ef0ed5018b846d1ad635b107795648
suprajgowda <suprajsupk@gmail.com>
2024-01-11 18:59:31 +0530

    skills done

--- src/theme2/Skills.tsx
+++ src/theme2/Skills.tsx
@@ -1,7 +1,213 @@
-import { Box, Typography } from "@mui/material";
 import React from "react";
+import { Box, Typography } from "@mui/material";
 import { DrawerAppBar } from "./HomePage";
 import Footer from "./Footer";
+
+const skillsList = [
+  {
+    title: "Real-World Impact:",
+    desc: "Our live projects go beyond the realm of hypothetical scenarios. Participants have the opportunity to work on projects that address actual challenges faced by industries today. From developing AI solutions to implementing blockchain technology, our projects have a tangible impact.",
+  },
+  {
+    title: "Diverse Domains:",
+    desc: "Choose from a diverse range of project domains, including Artificial Intelligence, Natural Language Detection, Blockchain, Machine Learning, Neural Networks, Cloud Computing, Web-Based Projects, and Aerospace and Thermal. Whether you're interested in revolutionizing communication or exploring the outer reaches of aerospace engineering, there's a project for you.",
+  },
+  {
+    title: "Guidance from Industry Experts:",
+    desc: "Our projects are guided by industry experts and seasoned professionals who bring invaluable insights to the learning experience. Benefit from mentorship that goes beyond textbooks, providing you with practical knowledge and a deeper understanding of the industry.",
+  },
+  {
+    title: "Collaborative Learning Environment:",
+    desc: "Engage in a collaborative learning environment where teamwork and communication are paramount. Our projects are designed to encourage participants to share ideas, solve challenges together, and foster a sense of camaraderie that extends beyond the project duration.",
+  },
+  {
+    title: "Career Acceleration:",
+    desc: "MAPLE is not just a platform for learning; it's a launchpad for your career. The skills acquired through our live projects set you apart in the competitive job market, giving you a distinct advantage when entering the professional world.",
+  },
+  {
+    title: "How It Works:",
+    desc: "The process is seamless. Participants choose a project domain of interest, engage in hands-on project work, collaborate with peers, and receive guidance from industry experts. The result? A portfolio of real-world projects that showcase your skills and demonstrate your ability to tackle complex challenges.",
+  },
+  {
+    title: "Ready to Ignite Your Potential?",
+    desc: "Join MAPLE and unlock the door to a world of innovation and growth. Our live projects are more than just learning experiences; they are opportunities to shape the future of technology. Ignite your curiosity, power your future. Welcome to Live Projects at MAPLE - where ideas turn into action, and learning becomes an adventure. Explore, create, and transform with us!",
+  },
+];
+
+const projectDomainList = [
+  {
+    header: "Artificial Intelligence (AI):",
+    key: "ai",
+    list: [
+      {
+        key1: "Description:",
+        value:
+          " Explore the vast landscape of AI, delving into machine learning algorithms, computer vision, and natural language processing.",
+      },
+      {
+        key1: "Project Examples:",
+        value:
+          " Develop chatbots, image recognition systems, and predictive analytics models.",
+      },
+      {
+        key1: "Skills Gained:",
+        value: " Programming in Python, machine learning, data analysis.",
+      },
+    ],
+  },
+  {
+    header: "Natural Language Detection:",
+    key: "nlp",
+    list: [
+      {
+        key1: "Description:",
+        value:
+          " Focus on projects that involve understanding and interpreting human language, opening doors to applications like chatbots and sentiment analysis.",
+      },
+      {
+        key1: "Project Examples:",
+        value:
+          " Build language detection systems, sentiment analysis models, and language translation tools.",
+      },
+      {
+        key1: "Skills Gained:",
+        value:
+          " Natural language processing, text analysis, algorithm development.",
+      },
+    ],
+  },
+  {
+    header: "Blockchain:",
+    key: "block",
+    list: [
+      {
+        key1: "Description:",
+        value:
+          " Immerse yourself in the secure and decentralized world of blockchain technology, exploring applications beyond cryptocurrencies.",
+      },
+      {
+        key1: "Project Examples:",
+        value:
+          " Develop blockchain solutions for supply chain management, healthcare, and finance.",
+      },
+      {
+        key1: "Skills Gained:",
+        value:
+          " Smart contract development, decentralized application (DApp) creation, blockchain architecture.",
+      },
+    ],
+  },
+  {
+    header: "Machine Learning:",
+    key: "machine",
+    list: [
+      {
+        key1: "Description:",
+        value:
+          " Journey into the fundamentals of machine learning, covering predictive modeling, data analysis, and algorithm implementation.",
+      },
+      {
+        key1: "Project Examples:",
+        value:
+          " Create machine learning models for prediction, classification, and clustering.",
+      },
+      {
+        key1: "Skills Gained:",
+        value:
+          " Data preprocessing, model training, evaluation, and optimization.",
+      },
+    ],
+  },
+  {
+    header: "Neural Network:",
+    key: "neural",
+    list: [
+      {
+        key1: "Description:",
+        value:
+          " Unleash the potential of neural networks, designing and training systems for applications like image recognition and speech processing.",
+      },
+      {
+        key1: "Project Examples:",
+        value: " Build and implement neural networks for various tasks.",
+      },
+      {
+        key1: "Skills Gained:",
+        value: " Deep learning, neural network architecture, model tuning.",
+      },
+    ],
+  },
+  {
+    header: "Cloud Computing:",
+    key: "cloud",
+    list: [
+      {
+        key1: "Description:",
+        value:
+          " Explore the possibilities of cloud computing, developing solutions that leverage the scalability and flexibility of cloud platforms.",
+      },
+      {
+        key1: "Project Examples:",
+        value:
+          " Design cloud-based applications, deploy services on cloud platforms.",
+      },
+      {
+        key1: "Skills Gained:",
+        value: " Cloud architecture, deployment, and management.",
+      },
+    ],
+  },
+  {
+    header: "Web-Based Projects:",
+    key: "web",
+    list: [
+      {
+        key1: "Description:",
+        value:
+          " Engage in full-stack development, creating interactive and user-friendly web applications.",
+      },
+      {
+        key1: "Project Examples:",
+        value:
+          " Build dynamic websites, e-commerce platforms, and web-based tools.",
+      },
+      {
+        key1: "Skills Gained:",
+        value: " Front-end and back-end development, database management.",
+      },
+    ],
+  },
+  {
+    header: "Aerospace and Thermal:",
+    key: "aero",
+    list: [
+      {
+        key1: "Description:",
+        value:
+          " Embark on projects that explore the challenges and innovations in aerospace engineering and thermal technology.",
+      },
+      {
+        key1: "Project Examples:",
+        value:
+          " Design aircraft systems, optimize thermal efficiency in engineering applications.",
+      },
+      {
+        key1: "Skills Gained:",
+        value: " Aerospace engineering principles, thermal system design.",
+      },
+    ],
+  },
+  {
+    header: "Robotics",
+    key: "Robotics",
+    list: [],
+  },
+  {
+    header: "3D Printer",
+    key: "3D Printer",
+    list: [],
+  },
+];
 
 export default function Skills() {
   return (
@@ -11,14 +217,187 @@ export default function Skills() {
       <Box
         sx={{
           mx: "10%",
+          mt: "8%",
         }}
       >
         <Typography
-          variant="h3"
-          component="div"
-          sx={{ pb: 4, fontWeight: 700 }}
+          variant="h4"
+          gutterBottom
+          sx={{
+            textAlign: "center",
+            color: "#384158",
+            left: { xs: "5%", md: "15%" },
+            fontSize: { xs: "25px", md: "50px" },
+            fontWeight: 700,
+          }}
         >
           Skills
+        </Typography>
+
+        <Box sx={{ display: "flex" }}>
+          <img
+            src="https://vamia.fi/wp-content/uploads/2023/06/brooke-cagle-uHVRvDr7pg-unsplash.jpg"
+            alt="Home Page"
+            style={{ width: "60%", marginTop: "2%" }}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              pl: 6,
+            }}
+          >
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ pb: 4, fontWeight: 700 }}
+            >
+              Overview of Live Projects at MAPLE
+            </Typography>
+            <Typography variant="h6" component="div" sx={{ fontWeight: 400 }}>
+              Embark on a transformative journey where theoretical knowledge
+              meets real-world application through our Live Projects at MAPLE.
+              Our projects are carefully crafted to provide participants with
+              hands-on experience in cutting-edge domains, fostering innovation,
+              and preparing them for the dynamic landscape of the tech industry.
+            </Typography>
+          </Box>
+        </Box>
+
+        {skillsList.map((skill) => {
+          return (
+            <Box sx={{ display: "flex", flexDirection: "column", my: 10 }}>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{ pb: 4, fontWeight: 700 }}
+              >
+                {skill.title}
+              </Typography>
+
+              <Typography variant="h6" component="div" sx={{ fontWeight: 400 }}>
+                {skill.desc}
+              </Typography>
+            </Box>
+          );
+        })}
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            my: 10,
+          }}
+        >
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{ pb: 4, fontWeight: 700 }}
+          >
+            Explore Our Project Domains in Detail
+          </Typography>
+
+          <Typography variant="h6" component="div" sx={{ fontWeight: 400 }}>
+            At MAPLE , we offer a spectrum of project domains, each designed to
+            immerse participants in the latest technologies and industry trends.
+            Choose your area of interest and dive into a hands-on learning
+            experience that goes beyond textbooks. Here's a detailed overview of
+            each project domain:
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            my: 6,
+          }}
+        >
+          {projectDomainList.map((proj) => {
+            return (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "43%",
+                  justifyContent: "space-between",
+                  alignItems: "start",
+                  border: "1px solid #4b4b4b29",
+                  borderTop: "5px solid #006983",
+                  boxShadow: "0px 3px 6px #00000029",
+                  mx: 4,
+                  my: 4,
+                }}
+              >
+                <img
+                  src="https://vamia.fi/wp-content/uploads/2023/06/brooke-cagle-uHVRvDr7pg-unsplash.jpg"
+                  alt="Home Page"
+                  style={{ width: "100%", height: "100%" }}
+                />
+
+                <Typography
+                  variant="h5"
+                  component="div"
+                  sx={{
+                    fontWeight: 700,
+                    textAlign: "left",
+                    width: "100%",
+                    px: 4,
+                    my: 2,
+                  }}
+                >
+                  {proj.header}
+                </Typography>
+
+                {proj.list.map((prjLs) => {
+                  return (
+                    <>
+                      <Typography
+                        variant="subtitle1"
+                        gutterBottom
+                        sx={{
+                          textAlign: "left",
+                          px: 4,
+                          mb: 1,
+                          fontWeight: 500,
+                        }}
+                      >
+                        {prjLs.key1}
+                      </Typography>
+                      <Typography
+                        variant="subtitle1"
+                        gutterBottom
+                        sx={{
+                          textAlign: "left",
+                          px: 4,
+                          mb: 3,
+                          fontWeight: 400,
+                        }}
+                      >
+                        {prjLs.value}
+                      </Typography>
+                    </>
+                  );
+                })}
+              </Box>
+            );
+          })}
+        </Box>
+
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ fontWeight: 400, mb: 6 }}
+        >
+          Each project domain at MAPLE offers a unique learning experience,
+          providing participants with the skills and knowledge necessary to
+          excel in their chosen field. Join us on this journey of exploration,
+          innovation, and growth!
         </Typography>
       </Box>
 
